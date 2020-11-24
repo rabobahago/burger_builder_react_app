5d5fa26259cc49134cfe800e469596c4127c0040
rabobahago <rabobahago@gmail.com>
2020-11-24 10:17:54 +0100

    basic navigation added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Toolbar.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 const Toolbar = () => (
   <header className="Toolbar">
     <div>MENU</div>
     <Logo />
-    <nav>...</nav>
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
 )
 export default Toolbar
