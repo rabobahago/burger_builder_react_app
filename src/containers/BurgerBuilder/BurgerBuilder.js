a9ec414e3bcc0f91674d84ed71bd1eec9cb0b641
rabobahago <rabobahago@gmail.com>
2020-12-15 15:50:39 +0100

    query params and strings added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -110,7 +110,19 @@ class BurgerBuilder extends Component {
     //     this.setState({ loading: false, purchasing: false })
     //     //console.log(err)
     //   })
-    this.props.history.push('/checkout')
+    const queryParams = []
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          '=' +
+          encodeURIComponent(this.state.ingredients[i]),
+      )
+    }
+    const queryString = queryParams.join('&')
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString,
+    })
   }
   render() {
     const disabledInfo = {
